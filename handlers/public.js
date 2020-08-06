284de69b50efea0fa3860d241a1e1ec9a0827251
bashir bashir <26bash@gmail.com>
2020-08-06 15:04:50 +0300

    finishing design

--- handlers/public.js
+++ handlers/public.js
@@ -3,28 +3,28 @@ const path = require("path");
 const missingHandler = require("./missing");
 
 const types = {
-  html: "text/html",
-  css: "text/css",
-  js: "application/javascript",
-  png: "image/png",
+   html: "text/html",
+   css: "text/css",
+   js: "application/javascript",
+   jpeg: "image/jpeg",
 };
 
 function publicHandler(request, response) {
-  const urlArray = request.url.split(".");
-  const extension = urlArray[1];
-  const type = types[extension];
+   const urlArray = request.url.split(".");
+   const extension = urlArray[1];
+   const type = types[extension];
 
-  const filePath = path.join(__dirname, "..", request.url);
-  console.log(filePath);
-  fs.readFile(filePath, (error, file) => {
-    if (error) {
-      console.log(error);
-      missingHandler(request, response);
-    } else {
-      response.writeHead(200, { "content-type": type });
-      response.end(file);
-    }
-  });
+   const filePath = path.join(__dirname, "..", request.url);
+   console.log(filePath);
+   fs.readFile(filePath, (error, file) => {
+      if (error) {
+         console.log(error);
+         missingHandler(request, response);
+      } else {
+         response.writeHead(200, { "content-type": type });
+         response.end(file);
+      }
+   });
 }
 
 module.exports = publicHandler;
